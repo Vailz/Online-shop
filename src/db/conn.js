5a9e55628263d1e0679287a1aa1bec0c8b1ce815
Shevchenko Daniil <72701362+Vailz@users.noreply.github.com>
2022-12-27 18:12:37 +0300

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/shopRegistration").then(() => {
+mongoose.connect("mongodb+srv://Daniel:admin@cluster0.pnixtwv.mongodb.net/test?retryWrites=true&w=majority").then(() => {
     console.log(`connection successful`)
 }).catch((e) => {
     console.log(`no connection`)
